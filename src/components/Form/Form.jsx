01735a59866a3af577a3f38dc05c12a97c919804
YuliaLugovaya <yulialugovayaphoto@gmail.com>
2023-05-22 12:54:54 +0300

    feat: add useContext

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { globalContext } from '../../contexts/globalContext';
 
-function Form({text, setText, handleSubmit}) {
+function Form() {
+
+  const { text, setText, handleSubmit } = useContext(globalContext);
+
   return (
     <section className='task add'>
-          <h2 className='subtitle'>Add task</h2>
-          <form className='mb-3 add__field' onSubmit={handleSubmit}>
-            <label htmlFor="exampleInput" className="form-label"></label>
-            <input
-              className="form-control"
-              placeholder="Write task"
-              onChange={(event) => setText(event.target.value)}
-              value={text}
-            />
-            <button type="submit" className="btn btn-primary">Add</button>
-          </form>
-        </section>
+      <h2 className='subtitle'>Add task</h2>
+      <form className='mb-3 add__field' onSubmit={handleSubmit}>
+        <label htmlFor="exampleInput" className="form-label"></label>
+        <input
+          className="form-control"
+          placeholder="Write task"
+          onChange={(event) => setText(event.target.value)}
+          value={text}
+        />
+        <button type="submit" className="btn btn-primary">Add</button>
+      </form>
+    </section>
   );
 }
 
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,49 +1,16 @@
-import { useState } from 'react';
 import React from 'react';
 import TaskList from '../TaskList/TaskList'
 import Form from '../Form/Form'
-import { useLocalStorage } from '../../hooks/useLS'
 
 
 function Main(props) {
-
-  const [list, setList] = useLocalStorage('tasks', []);
-  const [text, setText] = useState('');
-  const [checked, setChecked] = useState(false);
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    const post = {
-      text,
-      id: Date.now(),
-      status: false
-    }
-    setList((prev) => [...prev, post]);
-    setText('');
-  }
-
-  function changeStatus(id) {
-    setChecked(!checked);
-    setList(
-      list.map((el) => {
-        if (el.id === id) {
-          el.status = !el.status;
-        }
-        return el;
-      })
-    );
-  }
-
-  function removeTask(id) {
-    setList(list.filter((el) => id !== el.id));
-  }
 
   return (
     <>
       <main className='main'>
         <h1 className='title'>Create your personal ToDo-list</h1>
-        <Form text={text} setText={setText} handleSubmit={handleSubmit} />
-        <TaskList list={list} changeStatus={changeStatus} checked={checked} removeTask={removeTask}/>
+        <Form />
+        <TaskList />
       </main>
     </>
   );
